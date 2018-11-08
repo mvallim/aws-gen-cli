84636a7759f6b351593aa2b85ba2a90f7ca2dd67
Marcos Vallim <mvallim@ciandt.com>
2018-11-08 12:34:22 -0200

    0.0.12

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
  
 setupOptions = dict(
     name='awsgen',
-    version='0.0.11',
+    version='0.0.12',
     install_requires=[
         'boto3>=1.9.35',
         'botocore>=1.12.16',
