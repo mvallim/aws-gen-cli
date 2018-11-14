18e6083c0d7bd2c90e78cc3f36ba8cb50fd7c477
Marcos Vallim <mvallim@ciandt.com>
2018-11-14 12:58:01 -0200

    Update README.md

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setupOptions = dict(
     name='awsgen',
-    version='0.0.14',
+    version='0.0.16',
     install_requires=[
         'boto3>=1.9.35',
         'botocore>=1.12.16',
